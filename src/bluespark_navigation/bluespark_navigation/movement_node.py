d9d4b1b9ea37d96a86ea06408663242d7291f4b6
JaCier8 <jappa414@gmail.com>
2026-03-13 00:34:58 +0100

    Fixed bugs for MVP movement_node

--- src/bluespark_navigation/bluespark_navigation/movement_node.py
+++ src/bluespark_navigation/bluespark_navigation/movement_node.py
@@ -2,6 +2,7 @@ import rclpy
 from docutils.nodes import target
 from rclpy.node import Node
 
+from math import hypot
 from bluespark_interfaces.msg import DetectedObjectArray
 from bluespark_interfaces.srv import SetRCOverride
 
@@ -83,42 +84,65 @@ class MovementNode(Node):
 
         self.get_logger().info("Movement node started.")
 
+        self.stop_everything()
+
+    def stop_and_search(self):
+        self.current_state = self.STATE_SEARCHING
+        self.control_state.set_pwm("surge", STOP_PWM)
+        self.control_state.set_pwm("heave", STOP_PWM)
+        self.control_state.set_pwm("yaw", 1550)
+        changes = self.control_state.get()
+        for axis, pwm in changes.items():
+            self.send_rc_override(axis, pwm)
+
     def send_rc_override(self, axis, pwm_value):
         self.control_clients[axis].call_async(SetRCOverride.Request(pwm_value=pwm_value))
         self.get_logger().info(f"Sent RC override for {axis} to {pwm_value}")
+
+    def stop_everything(self):
+        for axis in self.control_clients.keys():
+            self.send_rc_override(axis, STOP_PWM)
 
     def vision_callback(self, msg):
         """
         Main logic of the node, the P algorithm.
         It is split for states: Centering and approaching
         and we do only one at the time
-        TODO Narazie zakładamy że widzi obiekt od poćzątku
-         i ten który jest tym pierwszy, do tego płynie
-        TODO Zrobić żeby płynął do bramki
+        TODO Aktualnie czeka aż zoabczy osobę i jak zoaczy to do niej płynie
+        TODO Zrobić żeby płynął do bramki i potem jeszcze nakierowywał na jej środek
         """
 
-        if len(msg.objects) == 0: self.logger.info("No objects detected")
+        if len(msg.objects) == 0:
+            self.get_logger().info("No objects detected")
+            self.stop_and_search()
+            return
 
-        target = msg.objects[0]
+        for obj in msg.objects:
+            if obj.label == "person":
+                target = obj
+                break
+
+        if target is None:
+            self.get_logger().info("No person detected among objects")
+            self.stop_and_search()
+            return
 
         TARGET_DISTANCE = 1.0
-        DEADBAND_ANGLE = 5.0
+        DEADBAND_ANGLE = 10.0
 
         # TODO tweak P parameters
         Kp_yaw = 4.0
-        Kp_pitch = 4.0
-        Kp_roll = 4.0
         Kp_heave = 4.0
         Kp_surge = 100.0
+        Kp_pitch = 4.0
+        Kp_roll = 4.0
         Kp_sway = 4.0
 
-        # TODO sanity check Maciek
         error_yaw = target.cam_h_angle_deg
         error_heave = target.cam_v_angle_deg
 
-        # TODO zmienić na fancy funckje wylicznaia tej odleggłości z 3 zmiennych i ogniskowej
-        # TODO 2 ALBO NIE XD
-        error_dist = target.pos_z - TARGET_DISTANCE
+        curr_dist = hypot(target.pos_x, target.pos_y, target.pos_z)
+        error_dist = curr_dist - TARGET_DISTANCE
 
         is_centered = ((abs(error_yaw) < DEADBAND_ANGLE)
                        and (abs(error_heave) < DEADBAND_ANGLE))
@@ -146,6 +170,7 @@ class MovementNode(Node):
             else:
                 self.control_state.set_pwm("heave", 1500)
 
+        # TODO Decide weather to always approach a bit or not
         # APPROACHING
         elif self.current_state == self.STATE_APPROACHING:
             self.control_state.set_pwm("yaw", STOP_PWM)
